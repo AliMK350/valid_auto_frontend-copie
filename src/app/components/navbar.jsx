a01acff4405c331c7b9b34b265709571359299da
Ali Mejber <alimejber055@gmail.com>
2025-05-10 15:31:13 +0100

    v9

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -29,10 +29,10 @@ export default function Navbar() {
               <small>0770145404</small>
             </div>
             <div className="h-100 d-inline-flex align-items-center">
-              <button className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-facebook-f"></i></button>
-              <button className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-instagram"></i></button>
-              <button className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-twitter"></i></button>
-              <button className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-linkedin-in"></i></button>
+              <button type="button" className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-facebook-f"></i></button>
+              <button type="button" className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-instagram"></i></button>
+              <button type="button" className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-twitter"></i></button>
+              <button type="button" className="btn btn-sm-square bg-white me-1" style={{ color: '#d81313' }}><i className="fab fa-linkedin-in"></i></button>
             </div>
           </div>
         </div>
@@ -41,12 +41,12 @@ export default function Navbar() {
       {/* Main Navbar */}
       <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
         <div className="container-fluid">
-          <button className="navbar-brand d-flex align-items-center px-4 px-lg-5 btn" style={{ border: 'none', background: 'none' }}>
+          <div className="navbar-brand d-flex align-items-center px-4 px-lg-5" style={{ cursor: 'pointer' }}>
             <h2 className="m-0" style={{ color: '#d81313' }}>
               <FontAwesomeIcon icon={faCar} className="me-3" />
               ValidAuto
             </h2>
-          </button>
+          </div>
 
           <button
             className="navbar-toggler me-4"
@@ -62,11 +62,12 @@ export default function Navbar() {
 
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <div className="navbar-nav ms-auto p-4 p-lg-0">
-              <button className="nav-item nav-link btn" style={{ background: 'none', border: 'none' }}>Accueil</button>
-              <button className="nav-item nav-link btn" style={{ background: 'none', border: 'none' }}>Nos services</button>
-              <button className="nav-item nav-link btn" style={{ background: 'none', border: 'none' }}>Contact</button>
+              <button type="button" className="nav-item nav-link btn" style={{ background: 'none', border: 'none' }}>Accueil</button>
+              <button type="button" className="nav-item nav-link btn" style={{ background: 'none', border: 'none' }}>Nos services</button>
+              <button type="button" className="nav-item nav-link btn" style={{ background: 'none', border: 'none' }}>Contact</button>
               <div className="d-lg-none mt-3">
                 <button
+                  type="button"
                   className="btn w-100"
                   style={{ backgroundColor: '#d81313', color: '#fff' }}
                 >
@@ -76,6 +77,7 @@ export default function Navbar() {
             </div>
             <div className="d-none d-lg-block">
               <button
+                type="button"
                 className="btn py-4 px-lg-5"
                 style={{ backgroundColor: '#d81313', color: '#fff' }}
               >
